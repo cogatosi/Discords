d441156433e87ac4cb11277b8e1a493e00c07a70
cogatosi <kazzystarve@gmail.com>
2026-04-29 21:00:00 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "Creator Cogato Playing Minecraft",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 1, 
       },
     ],
   },
